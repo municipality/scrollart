2dc997c7cd3c4065738389f8a64c49cd5dc2f93e
Brian <amunicipality@gmail.com>
2016-06-06 17:14:39 -0700

    fixing tetris top

--- app/tetris.component.ts
+++ app/tetris.component.ts
@@ -4,14 +4,17 @@ import {Component} from 'angular2/core';
     selector: 'tetris',
     template: `
         <div class="tetris-container">
-            <img #t class="tetris-top" (load)="handleTop(t)" src="images/tetris-top.png">
+
             <img #b class="tetris-bottom" (load)="handleBottom(b)" src="images/tetris-bottom-small.jpg">
         </div>
+        <img #t class="tetris-top" (load)="handleTop(t)" src="images/tetris-top.png">
     `
 })
 export class Tetris {
     t : any;
     b : any;
+    container;
+    tetris;
     constructor () {
 
     }
@@ -19,22 +22,49 @@ export class Tetris {
     handleTop (t) {
         this.t = t;
         var me = this;
-        var start = (me.t.height + 40) * -1;
-        if(me.t.style.top == "") {
-            me.t.style.top = start + "px";
-        }
+        var start = 0;
+
+
+        this.container = document.getElementsByClassName("tetris-container")[0];
+        this.container.style.height = window.innerHeight + "px";
+        this.tetris = document.getElementsByTagName("tetris")[0];
 
         document.addEventListener("scroll", (e) => {
-            //total height of the document
-            var totalHeight = document.body.scrollHeight - document.body.clientHeight;
-            //current scroll of the document
-            var currentHeight = document.body.scrollTop;
+            var rect = this.tetris.getBoundingClientRect();
+            console.log(rect);
+            if(rect.top > 0) {
+                this.container.style.position = "absolute";
+                this.container.style.top = "0";
 
-            me.t.style.top = start + (document.body.clientHeight * (currentHeight * 1.0 / totalHeight)) + "px";
-        })
+                t.style.position = "absolute";
+                t.style.bottom = this.tetris.offsetHeight;
+                t.style.bottom = "";
+            }
+            else if (rect.bottom <= window.innerHeight) {
+                this.container.style.position = "absolute";
+                this.container.style.bottom = "0";
+
+                t.style.position = "absolute";
+                t.style.bottom = 0;
+                t.style.top = "";
+            }
+            else {
+                this.container.style.position = "fixed";
+
+                t.style.position = "fixed";
+                console.log(this.tetris.x);
+                t.style.bottom = window.innerHeight * ((this.tetris.offsetHeight - ( window.pageYOffset))/(this.tetris.offsetHeight - t.offsetHeight));
+            }
+
+
+        });
+
+        window.addEventListener("resize", (e) => {
+        });
     }
 
     handleBottom (b) {
+        document.getElementById("app-loading-mask").style.display = "none";
         // this.b = b;
         // var me = this;
         // var top = me.b.offsetTop;
